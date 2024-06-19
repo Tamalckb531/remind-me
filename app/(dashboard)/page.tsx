7bb4fd70c84ca9774f2b77d4663acf8a5553fa46
Tamal <ckbtamaldipnew@gmail.com>
2024-06-19 21:30:32 +0600

    Add createCollectionBtn and sheet

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,8 @@
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import SadFace from "@/components/icons/sadFace";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { Suspense } from "react";
 
@@ -7,6 +11,9 @@ export default function Home() {
     <>
       <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading collections...</div>}>
+        <CollectionList />
       </Suspense>
     </>
   );
@@ -19,7 +26,7 @@ async function WelcomeMsg() {
     return <div>error</div>;
   }
   return (
-    <div className=" flex w-full justify-center">
+    <div className=" flex w-full justify-center mb-12">
       <h1 className=" text-4xl font-bold">
         Welcome, <br /> {user.firstName} {user.lastName}
       </h1>
@@ -29,11 +36,35 @@ async function WelcomeMsg() {
 
 function WelcomeMsgFallback() {
   return (
-    <div className=" flex w-full justify-center">
+    <div className=" flex w-full justify-center mb-12">
       <h1 className=" text-4xl font-bold">
         <Skeleton className=" w-[180px] h-[36px] mb-3" />
         <Skeleton className=" w-[360px] h-[36px]" />
       </h1>
     </div>
   );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className=" flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
 }
